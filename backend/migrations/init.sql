52e39ef21bd2f0d4d0682abe37515e05b595cfac
omardoescode <omardoescode@gmail.com>
2025-10-11 09:00:29 +0300

    db: create the segment part in db schema

--- backend/migrations/init.sql
+++ backend/migrations/init.sql
@@ -6,6 +6,9 @@ BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'lang') THEN
     CREATE TYPE Lang AS ENUM ('abap', 'bat', 'bibtex', 'clojure', 'coffeescript', 'c', 'cpp', 'csharp', 'dockercompose', 'css', 'cuda-cpp', 'd', 'dart', 'diff', 'dockerfile', 'erlang', 'fsharp',  'go', 'groovy', 'handlebars', 'haml', 'haskell', 'html', 'ini', 'java', 'javascript', 'javascriptreact', 'json', 'jsonc', 'julia', 'latex', 'less', 'lua', 'makefile', 'markdown', 'objective-c', 'objective-cpp', 'ocaml', 'pascal', 'perl', 'perl6', 'php', 'plaintext', 'powershell', 'jade', 'pug', 'python', 'r', 'razor', 'ruby', 'rust', 'scss', 'sass', 'shaderlab', 'shellscript', 'slim', 'sql', 'stylus', 'svelte', 'swift', 'typescript', 'typescriptreact', 'tex', 'vb', 'vue', 'vue-html', 'xml', 'xsl', 'yaml');
   END IF;
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'segmenttype') THEN
+    CREATE TYPE SegmentType AS ENUM ('coding', 'debugging', 'ai-coding');
+  END IF;
 END$$;
 
 create table if not exists users (
@@ -16,6 +19,8 @@ create table if not exists users (
   is_deleted bool default false,
   timezone integer check (timezone between -12 and 14)
 );
+
+create index idx_users_active on users(user_id) where not is_deleted; -- for active non deleted users
 
 create table if not exists machine(
   machine_id serial primary key,
@@ -34,7 +39,7 @@ create table if not exists api_key (
 );
 
 create table if not exists projects (
-  project_path varchar(500),
+  project_path varchar(500) not null,
   project_name text generated always as (regexp_replace(project_path, '^.*[\\/]', '')) stored,
   user_id integer references users(user_id),
   primary key (user_id, project_path),
@@ -49,7 +54,7 @@ create table if not exists project_files (
   project_path varchar(255),
   foreign key (user_id, project_path) references projects(user_id, project_path) on delete cascade,
 
-  file_path varchar(500), -- This is the path relative to the project path
+  file_path varchar(500) not null, -- This is the path relative to the project path
   file_name text generated always as (regexp_replace(file_path, '^.*[\\/]', '')) stored,
   lang Lang,
 
@@ -62,3 +67,23 @@ create table if not exists project_files (
 CREATE UNIQUE INDEX if not exists unique_active_file_path
 ON project_files (user_id, project_path, file_path)
 WHERE deleted_at IS NULL AND superseded_by IS NULL;
+
+create table if not exists file_segments (
+  segment_id serial primary key,
+
+  file_id integer not null references project_files(file_id) on delete cascade,
+
+  -- Time 
+  start_time timestamp not null,
+  end_time timestamp not null,
+  duration_seconds integer generated always as (extract(epoch from (end_time - start_time))) stored,
+  constraint end_after_start check (end_time >= start_time),
+  unique (file_id, start_time, end_time),
+
+  -- Metadata
+  segment_type SegmentType,
+  human_line_changes integer,
+  ai_line_changes integer,
+  editor Editor,
+  machine_id integer references machine(machine_id)
+);
