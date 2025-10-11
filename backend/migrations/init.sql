a6988be4326a3de31ea80e8a9d1a005d63a618db
omardoescode <omardoescode@gmail.com>
2025-10-11 08:42:49 +0300

    feat: update the schemas to include file rename setup

--- backend/migrations/init.sql
+++ backend/migrations/init.sql
@@ -4,7 +4,7 @@ BEGIN
     CREATE TYPE editor AS ENUM ('vscode', 'unknown');
   END IF;
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'lang') THEN
-    CREATE TYPE lang AS ENUM ('abap', 'bat', 'bibtex', 'clojure', 'coffeescript', 'c', 'cpp', 'csharp', 'dockercompose', 'css', 'cuda-cpp', 'd', 'dart', 'diff', 'dockerfile', 'erlang', 'fsharp',  'go', 'groovy', 'handlebars', 'haml', 'haskell', 'html', 'ini', 'java', 'javascript', 'javascriptreact', 'json', 'jsonc', 'julia', 'latex', 'less', 'lua', 'makefile', 'markdown', 'objective-c', 'objective-cpp', 'ocaml', 'pascal', 'perl', 'perl6', 'php', 'plaintext', 'powershell', 'jade', 'pug', 'python', 'r', 'razor', 'ruby', 'rust', 'scss', 'sass', 'shaderlab', 'shellscript', 'slim', 'sql', 'stylus', 'svelte', 'swift', 'typescript', 'typescriptreact', 'tex', 'vb', 'vue', 'vue-html', 'xml', 'xsl', 'yaml');
+    CREATE TYPE Lang AS ENUM ('abap', 'bat', 'bibtex', 'clojure', 'coffeescript', 'c', 'cpp', 'csharp', 'dockercompose', 'css', 'cuda-cpp', 'd', 'dart', 'diff', 'dockerfile', 'erlang', 'fsharp',  'go', 'groovy', 'handlebars', 'haml', 'haskell', 'html', 'ini', 'java', 'javascript', 'javascriptreact', 'json', 'jsonc', 'julia', 'latex', 'less', 'lua', 'makefile', 'markdown', 'objective-c', 'objective-cpp', 'ocaml', 'pascal', 'perl', 'perl6', 'php', 'plaintext', 'powershell', 'jade', 'pug', 'python', 'r', 'razor', 'ruby', 'rust', 'scss', 'sass', 'shaderlab', 'shellscript', 'slim', 'sql', 'stylus', 'svelte', 'swift', 'typescript', 'typescriptreact', 'tex', 'vb', 'vue', 'vue-html', 'xml', 'xsl', 'yaml');
   END IF;
 END$$;
 
@@ -37,16 +37,28 @@ create table if not exists projects (
   project_path varchar(500),
   project_name text generated always as (regexp_replace(project_path, '^.*[\\/]', '')) stored,
   user_id integer references users(user_id),
-  started_at timestamp not null default now(),
-  primary key (user_id, project_path)
+  primary key (user_id, project_path),
+
+  started_at timestamp not null default now()
 );
 
 create table if not exists project_files (
+  file_id integer primary key,
+
   user_id integer references users(user_id),
   project_path varchar(255),
+  foreign key (user_id, project_path) references projects(user_id, project_path) on delete cascade,
+
   file_path varchar(500), -- This is the path relative to the project path
   file_name text generated always as (regexp_replace(file_path, '^.*[\\/]', '')) stored,
-  lang lang,
-  primary key (user_id, project_path, file_path),
-  foreign key (user_id, project_path) references projects(user_id, project_path) on delete cascade
+  lang Lang,
+
+  -- File rename setup
+  old_file_path varchar(500), 
+  superseded_by integer references project_files (file_id),
+  deleted_at timestamp
 );
+
+CREATE UNIQUE INDEX if not exists unique_active_file_path
+ON project_files (user_id, project_path, file_path)
+WHERE deleted_at IS NULL AND superseded_by IS NULL;
